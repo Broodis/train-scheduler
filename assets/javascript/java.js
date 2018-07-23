599660ad70a6e8da36231c87b294d660b8bbb63e
Broodis_Desktop <j.porter712@gmail.com>
2018-07-23 15:51:16 -0700

    fix append to table

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -20,7 +20,7 @@ $("#addTrainButton").on("click", function (event) {
     var trainFirst = moment($("#tFirst").val().trim(), "HH:mm:ss").format("HH:mm:ss");
     var trainFreq = $("#tFreq").val().trim();
 
-    // Creates local "temporary" object for holding employee data
+    // Creates local "temporary" object for holding train data
     var newTrain = {
         name: trainName,
         dest: trainDest,
@@ -28,7 +28,7 @@ $("#addTrainButton").on("click", function (event) {
         freq: trainFreq
     };
 
-    // Uploads employee data to the database
+    // Uploads train data to the database
     database.ref().push(newTrain);
 
     // Logs everything to console
@@ -44,4 +44,32 @@ $("#addTrainButton").on("click", function (event) {
     $("#tDest").val("");
     $("#tFirst").val("");
     $("#tFreq").val("");
+});
+
+// Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+  
+    // Store everything into a variable.
+    var trainName = childSnapshot.val().name;
+    var trainDest = childSnapshot.val().dest;
+    var trainFirst = childSnapshot.val().first;
+    var trainFreq = childSnapshot.val().freq;
+  
+    // Train Info
+    console.log(trainName);
+    console.log(trainDest);
+    console.log(trainFirst);
+    console.log(trainFreq);
+
+    // Create the new row
+  var newRow = $("<tr>").append(
+    $("<td>").text(trainName),
+    $("<td>").text(trainDest),
+    $("<td>").text(trainFirst),
+    $("<td>").text(trainFreq),
+  );
+
+  // Append the new row to the table
+  $(".schedTable > tbody").append(newRow);
 });
